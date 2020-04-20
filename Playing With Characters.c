124210f427d4288583f15eede292b19db1aaf0ea
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2020-04-20 21:01:42 +0600

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[2], s2[100001], s3[100001];
+    gets(s1);
+    gets(s2);
+    gets(s3);
+    puts(s1);
+    puts(s2);
+    puts(s3);
+    return 0;
+}
